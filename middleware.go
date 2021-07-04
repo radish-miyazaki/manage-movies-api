120d1202dbcc8178fa5bac162afb133815829504
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2021-07-04 19:25:29 +0900

    [ADD] jwtをcheckするためのmiddlewareの追加

--- middleware.go
+++ middleware.go
@@ -1,12 +1,85 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"github.com/pascaldekloe/jwt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
 
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")  // 全オリジンを許可（ドメイン）
 		w.Header().Set("Access-Control-Allow-Headers", "*") // 全Headerを許可（Content-Typeなど）
 		w.Header().Set("Access-Control-Allow-Methods", "*") // 全Methodを許可（GETなど）
+
+		// 次のHandlerにチェーンする
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) checkToken(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Authorization")
+
+		// Headerに含まれるAuthorizationの値を変数に切り出す
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			// could set an anonymous user
+		}
+
+		headerParts := strings.Split(authHeader, " ")
+
+		// 値が正しい構成になっているか
+		if len(headerParts) != 2 {
+			app.errorJSON(w, errors.New("invalid auth header"))
+			return
+		}
+
+		// 先頭に"Bearer"が含まれるか
+		if headerParts[0] != "Bearer" {
+			app.errorJSON(w, errors.New("unauthorized - no bearer"))
+			return
+		}
+
+		// tokenの値のみを切り出す
+		token := headerParts[1]
+
+		// tokenが不正なものでないかチェック
+		claims, err := jwt.HMACCheck([]byte(token), []byte(app.config.jwt.secret))
+		if err != nil {
+			app.errorJSON(w, errors.New("unauthorized - failed hmac check"))
+			return
+		}
+
+		// tokenが有効期限内かチェック
+		if !claims.Valid(time.Now()) {
+			app.errorJSON(w, errors.New("unauthorized - token expired"))
+			return
+		}
+
+		// tokenの発行者が正しいかチェック
+		if claims.Issuer != "my_domain.com" {
+			app.errorJSON(w, errors.New("unauthorized - invalid issuer"))
+			return
+		}
+
+		// tokenの想定利用者が正しいかチェック
+		if !claims.AcceptAudience("my_domain.com") {
+			app.errorJSON(w, errors.New("unauthorized - invalid audience"))
+			return
+		}
+
+		userID, err := strconv.ParseInt(claims.Subject, 10, 64)
+		if err != nil {
+			app.errorJSON(w, errors.New("unauthorized"))
+			return
+		}
+
+		log.Println(userID)
 
 		// 次のHandlerにチェーンする
 		next.ServeHTTP(w, r)
--- routes.go
+++ routes.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"context"
 	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
 	"net/http"
 )
 
+func (app *application) wrap(next http.Handler) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		ctx := context.WithValue(r.Context(), "params", ps)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+}
+
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
+	secure := alice.New(app.checkToken)
 
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
 
@@ -16,7 +26,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.getMovie)
 
 	// Create & Update HandleFunc
-	router.HandlerFunc(http.MethodPost, "/v1/admin/movie/edit", app.editMovie)
+	router.POST("/v1/admin/movie/edit", app.wrap(secure.ThenFunc(app.editMovie)))
 	router.HandlerFunc(http.MethodDelete, "/v1/admin/movie/delete/:id", app.deleteMovie)
 
 	router.HandlerFunc(http.MethodGet, "/v1/genres", app.getAllGenres)
